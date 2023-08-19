b29a657de9e84d5a78fdf7f4ff148e0d263ef2f5
Chad Gray <rchadgray@gmail.com>
2023-08-18 21:38:50 -0400

    only seems to work with ESPTOOLS 4.2.1

--- bin_maker.ps1
+++ bin_maker.ps1
@@ -1,7 +1,7 @@
 ﻿# replace the content of the next three variables
 
 $commandLine= '
-C:\Users\cgray.GRAY\AppData\Local\Arduino15\packages\esp32\tools\esptool_py\3.1.0/esptool.exe --chip esp32 --port COM7 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0xe000 C:\Users\cgray.GRAY\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.1/tools/partitions/boot_app0.bin 0x1000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_542949/OnStep.ino.bootloader.bin 0x10000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_542949/OnStep.ino.bin 0x8000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_542949/OnStep.ino.partitions.bin 
+C:\Users\cgray.GRAY\AppData\Local\Arduino15\packages\esp32\tools\esptool_py\4.2.1/esptool.exe --chip esp32 --port COM7 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_861300/OnStepX.ino.bootloader.bin 0x8000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_861300/OnStepX.ino.partitions.bin 0xe000 C:\Users\cgray.GRAY\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.6/tools/partitions/boot_app0.bin 0x10000 C:\Users\CGRAY~1.GRA\AppData\Local\Temp\arduino_build_861300/OnStepX.ino.bin 
 
 '
 
@@ -14,7 +14,7 @@ $manifestFileName = 'manifest_' + $filePrefix + '_' + $ver + '.json'
 $fileName = $filePrefix + '_' + $ver + '.bin'
 
 
-$finalCommand = $commandLine.replace('--port COM7 --baud 921600 --before default_reset --after hard_reset write_flash -z','merge_bin -o firmware/' + $fileName)
+$finalCommand = $commandLine.replace('--port COM7 --baud 921600 --before default_reset --after hard_reset write_flash -z','merge_bin -o ' + $fileName)
 
 $finalCommand | Invoke-Expression
 
